d5fb1ea78954c779cfbf99e58ac8b56273456fca
Pragya1904 <pragya04r@gmail.com>
2022-06-10 14:53:36 +0530

    login page added

--- lib/screens/authentication/login_screen.dart
+++ lib/screens/authentication/login_screen.dart
@@ -45,14 +45,95 @@ class _MyLoginState extends State<MyLogin> {
           SizedBox(
             height:20
           ),
-          Container(
-            padding: EdgeInsets.only(top:20),
-            child: Text("Welcome Back",style:TextStyle(
-              color: Colors.blueAccent,
-              fontSize: 33,
+          SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.only(top:MediaQuery.of(context).size.height*0.5,left:35,right:35),
+              child: Column(
+                //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        suffixIcon: Icon(Icons.email_outlined,color: Colors.grey,),
+                        fillColor: Colors.grey.shade100,
+                        filled: true,
+                        hintText: "Email",
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide:const BorderSide(width:1,color: Colors.blueAccent),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide:const BorderSide(width:1,color:Colors.redAccent),
+                        )
+                    ),
+                  ),
+                  SizedBox(
+                      height:30
+                  ),
+                  TextFormField(
+                    obscureText:true,
+                    decoration: InputDecoration(
+                        suffixIcon: Icon(Icons.lock_open_outlined,color: Colors.grey),
+                        fillColor: Colors.grey.shade100,
+                        filled: true,
+                        hintText: "Password",
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide:const BorderSide(width:1,color: Colors.blueAccent),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide:const BorderSide(width:1,color:Colors.redAccent),
+                        )
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Sign in",style: TextStyle(color: Colors.blueAccent,
+                        fontSize: 27, fontWeight: FontWeight.w500,
+                      )),
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.blueAccent,
+                        child: IconButton(
+                            onPressed: (){
+                              // Navigator.pushNamed(context, homescreen);
+                            },
+                            icon:Icon(Icons.arrow_forward,size: 30,)
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(onPressed:(){
+                        Navigator.pushNamed(context, "forgotPassword");
+                      },
+                        child: Text("Forgot Password",style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          fontSize: 13,
+                        ),),
+                      ),
+                      TextButton(onPressed: (){
+                        Navigator.pushNamed(context, "signup");
+                      },
+                          child:Text("Sign Up",style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontSize: 13, )))
+                    ],
+                  )
+                ],
+              ),
             ),
-            ),
-          ),
+          )
         ],
       ),
     ));
