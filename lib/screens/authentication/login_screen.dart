e8ac70997f9637095f7ced607188edc5d16510e5
Pragya1904 <pragya04r@gmail.com>
2022-06-10 12:44:36 +0530

    added few files

---
+++ lib/screens/authentication/login_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+//import 'package:FC62_TheAvengers/screens/authentication/register_screen.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+
+class MyLogin extends StatefulWidget {
+  const MyLogin({Key? key}) : super(key: key);
+
+  @override
+  State<MyLogin> createState() => _MyLoginState();
+}
+
+class _MyLoginState extends State<MyLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/screens/authentication/register_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MySignUp extends StatefulWidget {
+  const MySignUp({Key? key}) : super(key: key);
+
+  @override
+  State<MySignUp> createState() => _MySignUpState();
+}
+
+class _MySignUpState extends State<MySignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
