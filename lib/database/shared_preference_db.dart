8c41b1955d7e6f5420939962f110bcc68d830ef0
Ashish Kumar Gopalika <meashishgopalika@gmail.com>
2022-06-10 18:17:17 +0530

    Update shared_preference_db.dart

--- lib/database/shared_preference_db.dart
+++ lib/database/shared_preference_db.dart
@@ -1,1 +1,27 @@
-// TODO Implement this library.
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferenceDB{
+  static final Future<SharedPreferences> _sharedPreferences = SharedPreferences.getInstance();
+
+  static Future<bool> setValue(String valueName, String? valueData) async{
+    bool state = false;
+    SharedPreferences prefs = await _sharedPreferences;
+    await prefs.setString(valueName, valueData!).then((value) {
+      state = true;
+    });
+    return state;
+  }
+
+  static Future<String?> getValue(String valueName) async{
+    String? value='';
+    SharedPreferences prefs = await _sharedPreferences;
+    value = prefs.getString(valueName);
+    return value;
+  }
+
+  static void resetPreferences() async{
+    SharedPreferences prefs = await _sharedPreferences;
+    await prefs.clear();
+  }
+
+}
