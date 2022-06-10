74d989194c815e0a4b585ba54d299d80b7bec402
UttamBindal <ping.uttambindal@gmail.com>
2022-06-10 22:31:54 +0530

    Profice pic editer with image picker added

---
+++ lib/screens/profile/Address.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+class Address {
+  final String name;
+  final String phonenumber;
+  final String pincode;
+  final String house_name;
+  final String area;
+  final String landmark;
+  final String town;
+  final String state;
+  final String country;
+
+  const Address({
+    required this.name,
+    required this.phonenumber,
+    required this.pincode,
+    required this.house_name,
+    required this.area,
+    required this.landmark,
+    required this.town,
+    required this.state,
+    required this.country,
+  });
+}
--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,50 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:fc62_theavengers/screens/main.dart';
-import 'package:fc62_theavengers/services/email_auth.dart';
+import 'package:enduserapp/screens/main.dart';
+import 'package:enduserapp/services/email_auth.dart';
+import 'package:enduserapp/screens/profile/edit_addresses.dart';
+import 'package:enduserapp/screens/profile/edit_profile.dart';
+import 'package:enduserapp/screens/profile/delete_account.dart';
+import 'package:enduserapp/screens/profile/menu_item.dart';
 import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
-import 'package:fc62_theavengers/screens/profile/edit_addresses.dart';
-import 'package:fc62_theavengers/screens/profile/edit_profile.dart';
-import 'package:fc62_theavengers/screens/profile/delete_account.dart';
-import 'package:fc62_theavengers/screens/profile/menu_item.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     return SafeArea(
       child: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(vertical: 20),
         child: Column(
           children: [
-            //ProfilePic(),
             const SizedBox(height: 20),
             ProfileMenu(
               text: "Edit Profile",
               press: () => {
-              Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const EditProfile()),
-              ),
-            },
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) =>  const EditProfile()),
+                ),
+              },
             ),
             ProfileMenu(
               text: "Edit Addresses",
               press: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const EditAddresses()),
+                  MaterialPageRoute(
+                      builder: (context) =>  EditAddresses()),
                 );
               },
-
             ),
             ProfileMenu(
               text: "Delete Account",
               press: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const DeleteAccount()),
+                  MaterialPageRoute(
+                      builder: (context) => const DeleteAccount()),
                 );
               },
             ),
@@ -57,10 +56,29 @@ class ProfileScreen extends StatelessWidget {
             ProfileMenu(
               text: "Log Out",
               press: () {
-                EmailAuth.signOut();
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const MainScreen()),
+                showDialog<String>(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    title: const Text('Sign Out'),
+                    content: const Text('Do you want to Sign Out?'),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () {
+                          EmailAuth.signOut();
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const MainScreen()),
+                          );
+                        },
+                        child: const Text('Sign out'),
+                      ),
+                      TextButton(
+                        onPressed: () => Navigator.pop(context, 'Cancel'),
+                        child: const Text('Cancel'),
+                      ),
+                    ],
+                  ),
                 );
               },
             ),
@@ -71,11 +89,11 @@ class ProfileScreen extends StatelessWidget {
   }
 }
 
+
 _callNumber() async {
   String number = "+91123456789";
   await FlutterPhoneDirectCaller.callNumber(number);
 }
-
 
 
 
